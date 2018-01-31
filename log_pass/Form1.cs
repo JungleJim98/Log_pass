16701de8a80fbf93a5ca957691e851edd364e3d3
JungleJim98 <bloodamerika@gmail.com>
2018-01-31 15:18:57 +0300

    version 1.3

---
+++ log_pass/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace log_pass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            bool read;
+            SqlDataReader rdr = null;
+            string connection = "server=192.168.1.5;database=students;UID=kpv_student;password=GRAgLqO5";
+            string FirstName = textBox2.Text, LastName = textBox3.Text, MiddleName = textBox1.Text;
+            string query;
+            query = (MiddleName == "") ? ("select * from student where FirstName='" + FirstName + "' and LastName='" + LastName+"'") : ("select * from student where FirstName='" + FirstName + "' and LastName='" + LastName + "' and MiddleName='" + MiddleName + "'");
+            SqlConnection connect = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand(query, connect);
+
+            connect.Open();
+            rdr = cmd.ExecuteReader();
+            while (read = rdr.Read())
+            {
+                if (read == false)
+                    MessageBox.Show("Ошибка\nНет данных");
+                else
+                {
+                    string pass = rdr["password"].ToString(), log = rdr["Login"].ToString(), LN=rdr["LastName"].ToString(),FN=rdr["FirstName"].ToString(),MN=rdr["MiddleName"].ToString();
+                    string[] user = { LN, FN, MN, log, pass };
+                    dataGridView1.Rows.Add(user);
+                }
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString()=="Return")
+            {
+                button1_Click(this, EventArgs.Empty);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.Columns[0].Name = "Фамилия";
+            dataGridView1.Columns[1].Name = "Имя";
+            dataGridView1.Columns[2].Name = "Отчество";
+            dataGridView1.Columns[3].Name = "Login";
+            dataGridView1.Columns[4].Name = "Password";
+
+        }
+    }
+}
---
+++ log_pass/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("log_pass")]
+[assembly: AssemblyDescription("Программа для поиска студентов")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ЦТТиДО")]
+[assembly: AssemblyProduct("log_pass")]
+[assembly: AssemblyCopyright("BitConfig Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("102f2b7d-1f05-4ef8-a23b-52a556eb9ead")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
